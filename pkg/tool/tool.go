9b89d50f7a632f0c43c82ffbda16bdb836b23cb5
Kaiser925 <zhangliang925@icloud.com>
2022-09-29 17:59:59 +0800

    feat: add StringSegmentFunc

--- pkg/tool/tool.go
+++ pkg/tool/tool.go
@@ -1,6 +1,16 @@
 package tool
 
-// GetRootDir return the root dir of name.
-func GetRootDir(name string) string {
-	return name
+import "strings"
+
+type StringSegmentFunc func(key string, start int) (segment string, nextIndex int)
+
+func PathSegment(path string, start int) (segment string, next int) {
+	if len(path) == 0 || start < 0 || start > len(path)-1 {
+		return "", -1
+	}
+	end := strings.IndexRune(path[start+1:], '/') // next '/' after 0th rune
+	if end == -1 {
+		return path[start:], -1
+	}
+	return path[start : start+end+1], start + end + 1
 }
--- pkg/tool/tool_test.go
+++ pkg/tool/tool_test.go
@@ -1,1 +1,40 @@
 package tool
+
+import "testing"
+
+func TestPathSegmenter(t *testing.T) {
+	cases := []struct {
+		key     string
+		parts   []string
+		indices []int // indexes to use as next start, in order
+	}{
+		{"", []string{""}, []int{-1}},
+		{"/", []string{"/"}, []int{-1}},
+		{"static_file", []string{"static_file"}, []int{-1}},
+		{"/users/scott", []string{"/users", "/scott"}, []int{6, -1}},
+		{"users/scott", []string{"users", "/scott"}, []int{5, -1}},
+		{"/users/ramona/", []string{"/users", "/ramona", "/"}, []int{6, 13, -1}},
+		{"users/ramona/", []string{"users", "/ramona", "/"}, []int{5, 12, -1}},
+		{"//", []string{"/", "/"}, []int{1, -1}},
+		{"/a/b/c", []string{"/a", "/b", "/c"}, []int{2, 4, -1}},
+	}
+
+	for _, c := range cases {
+		partNum := 0
+		for prefix, i := PathSegment(c.key, 0); ; prefix, i = PathSegment(c.key, i) {
+			if prefix != c.parts[partNum] {
+				t.Errorf("expected part %d of key '%s' to be '%s', got '%s'", partNum, c.key, c.parts[partNum], prefix)
+			}
+			if i != c.indices[partNum] {
+				t.Errorf("in iteration %d, expected next index of key '%s' to be '%d', got '%d'", partNum, c.key, c.indices[partNum], i)
+			}
+			partNum++
+			if i == -1 {
+				break
+			}
+		}
+		if partNum != len(c.parts) {
+			t.Errorf("expected '%s' to have %d parts, got %d", c.key, len(c.parts), partNum)
+		}
+	}
+}
